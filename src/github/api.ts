b76253b67600081c4684d895e697b7ea53e17d53
Felipe Armoni <felipe.armoni@gmail.com>
2023-03-01 17:35:29 -0300

    Added github api

---
+++ src/github/api.ts
@@ -0,0 +1,79 @@
+import { Octokit } from 'octokit';
+
+async function authenticate() {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  return octokit;
+}
+
+function getPublicGithubRepositories(octokit: Octokit) {
+  const gql = octokit.graphql; // Must do this to preserve syntax highlighting for some reason.
+
+  return () =>
+    gql(`
+    query Repositories {
+      search(query: "stars:>0", type: REPOSITORY, first: 10) {
+        repositoryCount
+        edges {
+          cursor
+          node {
+            ... on Repository {
+              id
+              name
+              owner {
+                id
+                url
+                login
+              }
+              url
+              description
+              createdAt
+              updatedAt
+              forkCount
+              stargazerCount
+            }
+          }
+        }
+      }
+    }
+  `);
+}
+
+function getUsersPerRepository(octokit: Octokit) {
+  const gql = octokit.graphql; // Must do this to preserve syntax highlighting for some reason.
+
+  return (name: string, owner: string) =>
+    gql(
+      `
+    query UsersPerRepository($name: String!, $owner: String!) {
+    repository(name: $name, owner: $owner) {
+      mentionableUsers(first: 100) {
+        edges {
+          cursor
+          node {
+            id
+            name
+            websiteUrl
+            url
+            login
+            bio
+          }
+        }
+      }
+    }
+  }
+  `,
+      { name, owner }
+    );
+}
+
+export async function getGithubApi() {
+  const octokit = await authenticate();
+
+  return {
+    getPublicGithubRepositories: getPublicGithubRepositories(octokit),
+    getUsersPerRepository: getUsersPerRepository(octokit),
+  };
+}
--- src/github/github.ts
+++ src/github/github.ts
@@ -1,81 +1,11 @@
-import { Octokit } from 'octokit';
-
-async function authenticate() {
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
-  });
-
-  return octokit;
-}
-
-function getPublicGithubRepositories(octokit: Octokit) {
-  const gql = octokit.graphql; // Must do this to preserve syntax highlighting for some reason.
-
-  return gql(`
-    query Repositories {
-      search(query: "stars:>0", type: REPOSITORY, first: 10) {
-        repositoryCount
-        edges {
-          cursor
-          node {
-            ... on Repository {
-              id
-              name
-              owner {
-                id
-                url
-                login
-              }
-              url
-              description
-              createdAt
-              updatedAt
-              forkCount
-              stargazerCount
-            }
-          }
-        }
-      }
-    }
-  `);
-}
-
-function getUsersPerRepository(octokit: Octokit) {
-  const gql = octokit.graphql; // Must do this to preserve syntax highlighting for some reason.
-
-  return (name: string, owner: string) =>
-    gql(
-      `
-    query UsersPerRepository($name: String!, $owner: String!) {
-    repository(name: $name, owner: $owner) {
-      mentionableUsers(first: 100) {
-        edges {
-          cursor
-          node {
-            id
-            name
-            websiteUrl
-            url
-            login
-            bio
-          }
-        }
-      }
-    }
-  }
-  `,
-      { name, owner }
-    );
-}
+import { getGithubApi } from './api';
 
 export async function getGithubData() {
-  const octokit = await authenticate();
+  const api = await getGithubApi();
 
-  const repositories = await getPublicGithubRepositories(octokit);
-  // pretty print json result on console
-  // console.log(JSON.stringify(repositories, null, 2));
+  const repositories = await api.getPublicGithubRepositories();
 
-  const usersPerRepository = await getUsersPerRepository(octokit)(
+  const usersPerRepository = await api.getUsersPerRepository(
     'electron',
     'electron'
   );
